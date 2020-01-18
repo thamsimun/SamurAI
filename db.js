82667c94f14545c9c8cd3216c616b5dda34f05a0
kayheen <kayheen@u.nus.edu>
2020-01-18 14:10:14 +0800

    edit db.js file

--- db.js
+++ db.js
@@ -1,20 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var firebase = require("firebase/app");
+var database = require("firebase/database");
+var firestore = require("firebase/firestore");
+var auth = require("firebase/auth");
+var functions = require("firebase/functions");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+var firebaseConfig = {
+    apiKey: "AIzaSyDQv2R4ijXaDooj7DiSh1WkR66PcuBA3FM",
+    authDomain: "samurai-e00ed.firebaseapp.com",
+    databaseURL: "https://samurai-e00ed.firebaseio.com",
+    projectId: "samurai-e00ed",
+    storageBucket: "samurai-e00ed.appspot.com",
+    messagingSenderId: "839463369964",
+    appId: "1:839463369964:web:1a06fe4c5b9c40d92c79c9",
+    measurementId: "G-Q2S9RY2YKH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
